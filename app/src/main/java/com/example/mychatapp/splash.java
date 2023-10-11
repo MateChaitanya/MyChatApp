6dea19ed05a5b3347c7f175e16599a554e1c966d
MateChaitanya <chaitanyamate02@gmail.com>
2023-10-11 23:14:12 +0530

    Initial commit

--- app/src/main/java/com/example/mychatapp/splash.java
+++ app/src/main/java/com/example/mychatapp/splash.java
@@ -2,7 +2,9 @@ package com.example.mychatapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class splash extends AppCompatActivity {
 
@@ -12,5 +14,13 @@ public class splash extends AppCompatActivity {
         setContentView(R.layout.activity_splash);
 
 
-    }
-}
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(splash.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },4000);
+            }
+        }
