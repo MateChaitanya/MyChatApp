1085469275cc7e9f81b8aac4793feb1a325bb0de
MateChaitanya <chaitanyamate02@gmail.com>
2023-10-12 21:20:24 +0530

    Initial Commit

--- app/src/main/java/com/example/mychatapp/splash.java
+++ app/src/main/java/com/example/mychatapp/splash.java
@@ -12,6 +12,7 @@ public class splash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        getSupportActionBar().hide();
 
 
         new Handler().postDelayed(new Runnable() {
